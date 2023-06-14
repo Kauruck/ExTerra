52f5a4adae42d7d4fb2966e0612dc3caf3224b7d
Kauruck <39699490+Kauruck@users.noreply.github.com>
2023-06-14 12:38:01 +0200

    Added the ExTerraReloadable to the DataEventHandler. I forgot this one.

--- src/main/java/com/kauruck/exterra/data/DataEventHandler.java
+++ src/main/java/com/kauruck/exterra/data/DataEventHandler.java
@@ -3,6 +3,7 @@ package com.kauruck.exterra.data;
 import com.kauruck.exterra.ExTerra;
 import com.kauruck.exterra.api.recipes.ExTerraRecipeManager;
 import com.kauruck.exterra.data.loader.ShapeReloadListener;
+import com.kauruck.exterra.modules.ExTerraReloadableResources;
 import com.kauruck.exterra.recipes.ConversionRecipe;
 import net.minecraftforge.event.AddReloadListenerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -11,11 +12,9 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = ExTerra.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class DataEventHandler {
 
-    public static ExTerraRecipeManager<ConversionRecipe> conversionRecipes;
     @SubscribeEvent
     public static void DataPackReloadEvent(AddReloadListenerEvent event){
         event.addListener(new ShapeReloadListener());
-        conversionRecipes = new ExTerraRecipeManager<>(event.getConditionContext(), "conversion" , ExTerra.getResource("conversion"));
-        event.addListener(conversionRecipes);
+        event.addListener(ExTerraReloadableResources.INSTANCE);
     }
 }
